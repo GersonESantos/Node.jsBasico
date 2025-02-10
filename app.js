ed936e701c2fa91eb129f67587dfd235be33159c
GersonESS <gebhsantos@gmail.com>
2025-02-09 21:17:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,8 @@ var fetch = require('node-fetch');
 var app = express();
 
 // Rota
-app.get('/', function(req, res){
-
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(resposta => resposta.json())
-    .then(resposta => console.table(resposta))
-
-    res.end();
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
 listen(3000, () => {
